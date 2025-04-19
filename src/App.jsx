a08b5b4959da40ca6b809fbf4b0c70f8dabb3d86
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-04-19 15:59:43 -0300

    Estrutura dos cards terminada e fazendo a requisição no "data.json" para pegar as informações.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Components
-import Card from './components/Card'
 import Header from './components/Header'
+import Card from './components/Card'
 
 // Utils
 import { useState, useEffect } from 'react'
@@ -8,13 +8,23 @@ import './App.css'
 
 function App() {
   const [activeFilter, setActiveFilter] = useState('All')
+  const [data, setData] = useState([])
 
   const handle_filter_click = (filter) => {
     setActiveFilter(filter)
   }
 
   useEffect(() => {
-
+    fetch('data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(json_data => {
+        setData(json_data)
+      })
   }, [])
 
   return (
@@ -50,11 +60,31 @@ function App() {
         </nav>
 
         <section className='cards_section'>
-
+          {data.length != 0 && data.map((card) => (
+            <Card
+              key={card.name}
+              icon={card.logo}
+              name={card.name}
+              description={card.description}
+              is_active={card.isActive}
+            />
+          ))}
         </section>
       </main>
     </>
   )
 }
+
+// Número de extensões = 12
+
+// Bootstrap
+{/* <div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+  <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+</div>
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+  <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+</div> */}
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,32 @@
+import './card.css'
+
 function Card({
     icon = '',
-    title = '',
-    description = ''
+    name = '',
+    description = '',
+    is_active
 }) {
+    return (
+        <div className="card">
+            <div className="content">
+                <img src={icon} alt="Icon" />
+                
+                <div className='text'>
+                    <strong>{name}</strong>
+                    <br />
+                    <p>{description}</p>
+                </div>
+            </div>
 
+            <div className="options">
+                <button>
+                    Remove
+                </button>
+
+                {is_active && <p>Teste</p>}
+            </div>
+        </div>
+    )
 }
 
 export default Card 
