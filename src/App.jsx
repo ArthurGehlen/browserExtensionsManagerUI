6f2d09d50eabf6b194b8c2149ffa7b877b4a8102
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-04-20 20:54:13 -0300

    Criando o botão de "check".

--- src/App.jsx
+++ src/App.jsx
@@ -75,16 +75,4 @@ function App() {
   )
 }
 
-// Número de extensões = 12
-
-// Bootstrap
-{/* <div class="form-check form-switch">
-  <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-  <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
-</div>
-<div class="form-check form-switch">
-  <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
-  <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
-</div> */}
-
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,19 @@
 import './card.css'
+import { useState } from 'react'
 
 function Card({
     icon = '',
     name = '',
     description = '',
-    is_active
+    is_active =  false
 }) {
+    const [isActive, setIsActive] = useState(is_active)
+
     return (
         <div className="card">
             <div className="content">
                 <img src={icon} alt="Icon" />
-                
+
                 <div className='text'>
                     <strong>{name}</strong>
                     <br />
@@ -23,7 +26,12 @@ function Card({
                     Remove
                 </button>
 
-                {is_active && <p>Teste</p>}
+                <div 
+                className={`check ${isActive ? 'check_container_checked' : 'check_container__not_checked'}`}
+                tabIndex='0'>
+                    <input type="checkbox" id="check" onClick={() => setIsActive(!isActive)} />
+                    <label htmlFor="check" className={isActive ? 'label_checked' : 'label_not_checked'}></label>
+                </div>
             </div>
         </div>
     )
